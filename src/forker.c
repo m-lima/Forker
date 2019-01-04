e98327512179e0f2a11cf16fe2bbc4d337ce05f3
Marcelo Lima <marcelo@telenordigital.com>
2019-01-04 18:02:37 +0100

    Use system() for process call

--- src/forker.c
+++ src/forker.c
@@ -3,127 +3,138 @@
 #include <string.h>
 #include <unistd.h>
 
-#define VERBOSE
+// #define VERBOSE
 
-struct Command {
-  int index;
-  pid_t pid;
-};
+char ** extractCommands(int argc, char * argv[], int * outputCmdCnt) {
+  int argStart = 1;
+  char * separator = "%";
 
-char * separator = "%";
-int argStart = 1;
-int cmdCount = 0;
-int cmdArrayCount = 1;
-struct Command * commands;
-
-void getSeparator(char * arg) {
-  if (arg[0] == '-' && arg[1] == 's') {
-    if (arg[2] == 0) {
-      fputs("Expected a separator\n", stderr);
+  if (argv[1][0] == '-' && argv[1][1] == 's') {
+    if (argv[1][2] == 0) {
+      fputs("[31m -- Expected a separator[m\n", stderr);
       exit(EXIT_FAILURE);
     }
 
-    separator = arg + 2;
+    separator = argv[1] + 2;
     argStart++;
   }
-}
 
-void createCommand(int index) {
-  if (argStart == index) {
-    argStart++;
-    return;
+#ifdef VERBOSE
+  printf("[35m -- Separator: %s[m\n", separator);
+#endif
+
+  char ** cmds = malloc(2);
+  if (!cmds) {
+    perror("[31m -- Failed to allocate memory for commands strings[m");
+    exit(EXIT_FAILURE);
   }
 
-  if (cmdCount == cmdArrayCount) {
-    cmdArrayCount <<= 1;
-    commands = realloc(commands, cmdArrayCount * sizeof(struct Command));
-    if (!commands) {
-      perror("Failed to allocate memory for command strings");
-      exit(EXIT_FAILURE);
+  int cmdAlloc = 2;
+  int cmdCnt = 0;
+
+  cmds[cmdCnt] = malloc(256);
+  if (!cmds[cmdCnt]) {
+    perror("[31m -- Failed to allocate memory for argument buffer[m");
+    exit(EXIT_FAILURE);
+  }
+
+  int bufferAlloc = 256;
+  int bufferLength = 0;
+  cmds[cmdCnt][0] = 0;
+
+  for (int i = argStart; i < argc; i++) {
+    size_t argLength = strlen(argv[i]);
+    if (argLength == 0) {
+      continue;
     }
+
+    if (strcmp(argv[i], separator) == 0) {
+      if (bufferLength > 0) {
+        if (cmdCnt >= cmdAlloc) {
+          cmds = realloc(cmds, cmdAlloc + 2);
+          cmdAlloc += 2;
+        }
+
+        cmdCnt++;
+        cmds[cmdCnt] = malloc(256);
+        if (!cmds[cmdCnt]) {
+          perror("[31m -- Failed to allocate memory for argument buffer[m");
+          exit(EXIT_FAILURE);
+        }
+
+        bufferAlloc = 256;
+        bufferLength = 0;
+        cmds[cmdCnt][0] = 0;
+      }
+      continue;
+    }
+
+    if (argLength + bufferLength + 1 >= bufferAlloc) {
+      cmds[cmdCnt] = realloc(cmds[cmdCnt], bufferAlloc + 256);
+      if (!cmds[cmdCnt]) {
+        perror("[31m -- Failed to allocate memory for argument buffer[m");
+        exit(EXIT_FAILURE);
+      }
+      bufferAlloc += 256;
+    }
+
+    if (bufferLength != 0) {
+      strcat(cmds[cmdCnt], " ");
+      bufferLength++;
+    }
+
+    strcat(cmds[cmdCnt], argv[i]);
+    bufferLength += argLength;
   }
 
-  commands[cmdCount].index = argStart;
-
-  cmdCount++;
-  argStart = index + 1;
+  *outputCmdCnt = cmdCnt + (cmds[cmdCnt][0] == 0 ? 0 : 1);
+  return cmds;
 }
 
 int main(int argc, char * argv[]) {
 
   if (argc < 2) {
-    fputs("Expected an executable\n", stderr);
+    fputs("[31m -- Expected an executable[m\n", stderr);
     exit(EXIT_FAILURE);
   }
 
-  getSeparator(argv[1]);
-  printf("Separator: %s\n", separator);
+  int cmdCnt = 0;
+  int pidCnt = 0;
+  char ** cmds = extractCommands(argc, argv, &cmdCnt);
 
-  commands = malloc(sizeof(struct Command));
-  if (!commands) {
-    perror("Failed to allocate memory for command strings");
+  if (cmdCnt == 0) {
+    fputs("[31m -- Expected an executable[m\n", stderr);
     exit(EXIT_FAILURE);
   }
 
-  for (int i = argStart; i < argc; ++i) {
-    if (strcmp(argv[i], separator) == 0) {
-      argv[i] = 0;
-      createCommand(i);
-    }
-  }
-  createCommand(argc);
-
-  if (cmdCount == 0) {
-    fputs("Expected an executable\n", stderr);
-    exit(EXIT_FAILURE);
-  }
+  pid_t * pids = malloc(cmdCnt * sizeof(pid_t));
 
 #ifdef VERBOSE
-  for (int i = 0; i < cmdCount; ++i) {
-    printf("Command %d:", i);
-    for (int j = commands[i].index; j < argc; ++j) {
-      if (argv[j] == 0) {
-        break;
-      }
-      printf(" %s", argv[j]);
-    }
-    puts("");
+  for (int i = 0; i < cmdCnt; ++i) {
+    printf("[35m -- Command %d: %s[m\n", i, cmds[i]);
   }
 #endif
 
-  for (int i = 0; i < cmdCount; ++i) {
-#ifdef VERBOSE
-    printf("[%s] Forking\n", argv[commands[i].index]);
-#endif
-
+  for (int i = 0; i < cmdCnt; ++i) {
     pid_t pid = fork();
 
     if (pid < 0) {
-      perror("Failed to fork");
-      exit(EXIT_FAILURE);
+      perror("[31m -- Failed to fork[m");
+      break;
     } else if (pid == 0) {
-#ifdef VERBOSE
-      printf("[%s] Starting\n", argv[commands[i].index]);
-#endif
-      execv(argv[commands[i].index], argv + commands[i].index);
-      free(commands);
-      exit(EXIT_SUCCESS);
+      exit(system(cmds[i]));
     } else {
-      printf("[%s] Started with PID %ld\n", argv[commands[i].index], (long) pid);
-      commands[i].pid = pid;
+      printf("[35m -- Started '%s' with PID %ld[m\n", cmds[i], (long) pid);
+      pids[pidCnt] = pid;
+      pidCnt++;
     }
   }
 
-  for (int i = 0; i < cmdCount; ++i) {
-#ifdef VERBOSE
-    printf("[%s] Waiting for PID %ld\n", argv[commands[i].index], (long) commands[i].pid);
-#endif
-
+  for (int i = 0; i < pidCnt; ++i) {
     int status = 0;
-    waitpid(commands[i].pid, &status, 0);
-    printf("[%s] Exited with status %d\n", argv[commands[i].index], status);
+    waitpid(pids[i], &status, 0);
+    printf("[35m -- '%s' exited with status %d[m\n", cmds[i], status);
   }
 
-  free(commands);
   exit(EXIT_SUCCESS);
 }
